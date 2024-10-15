2e504b5873b0c5a1b78f3673269a925ee83d0b78
Anshuraj09 <anshuraj2771@gmail.com>
2024-10-16 03:06:46 +0530

    updated

--- Test151023/Test1.cpp
+++ Test151023/Test1.cpp
@@ -100,9 +100,43 @@ void boxfill(int n){
         cout<<endl;
      }
 }
+void square(int n){
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+        if (i == 0 || i == n-1 || j == 0 || j == n-1) {
+                cout <<n-1<<" ";
+            }
+        else if (i == 1 || j == 1 || i==n-2 || j==n-2) {
+            cout<<n-2<<" ";
+        }
+        else if (i == 2 || j == 2 || i==n-3 || j==n-3) {
+            cout<<n-3<<" ";
+        }
+        else if (i == 3 || j == 3 || i==n-4 || j==n-4) {
+            cout<<n-4<<" ";
+        }
+            else {
+                cout << " ";
+            }
+        }
+        cout<<endl;
+   }
+}
+void square1(int n){
+    for(int i=0;i<2*n-1;i++){
+        for(int j=0;j<2*n-1;j++){
+            int top=i;
+            int left=j;
+            int right=(2*n-2)-j;
+            int down=(2*n-2)-i;
+            cout<<(n-min(min(top,down),min(left,right)));
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cout<<"Enter the pattern no. u want:";
     cin>>n;
-    boxfill(n);
+    square1(n);
 }
